7b8898d17f7f977d28efaff05ec185665c34bc0d
Pedro Hoppe <pedrohbh@users.noreply.github.com>
2022-11-19 20:35:43 -0300

    Quickfix geração

--- gerado/src/main/webapp/app/admin/produto/admin-produto.component.ts
+++ gerado/src/main/webapp/app/admin/produto/admin-produto.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 				<tr>
 					<td>{{ table }}</td>
 				</tr>
-			</table><td id="produto.id">{{ produto.id }}</td><td id="produto.name">{{ produto.name }}</td><td id="produto.preco">{{ produto.preco }}</td>
+			</table><td id="produto.id">{{ produto.id }}</td><td id="produto.name">{{ produto.name }}</td><td id="produto.preco">{{ produto.preco }}</td><td id="produto.estoque">{{ produto.estoque }}</td><td id="produto.categoria.name">{{ produto.categoria.name }}</td>
    
    <form>
    <input type="text" id="produto.nome" value="produto.nome" /><div id="nome.invalid" value="nome.invalid" /><div id="produto.url" value="produto.url" /><img src={produto.url} value="produto.url" /><input type="text" id="produto.url" value="produto.url" /><div id="url.invalid" value="url.invalid" /><input type="textarea" id="produto.descricao" value="produto.descricao" /><div id="descricao.invalid" value="descricao.invalid" /><input type="select" id="produto.categoria" value="produto.categoria" /><input type="number" id="produto.estoque" value="produto.estoque" /><div id="estoque.invalid" value="estoque.invalid" /><input type="number" id="produto.custo" value="produto.custo" /><div id="custo.invalid" value="custo.invalid" /><input type="number" id="produto.preco" value="produto.preco" /><div id="preco.invalid" value="preco.invalid" />
